77c885f0c9d0aeeb1d5352aa53c1448ecb511fe5
Ian Klatzco <iklatzco@gmail.com>
2023-04-08 22:49:49 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atprototools',
+    version='0.0.1',
+    description='tools for posting / deleting things from bsky, in python',
+    author='Ian Klatzco',
+    author_email='iklatzco@gmail.com',
+    url='https://github.com/ianklatzco/atprototools',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.22.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
